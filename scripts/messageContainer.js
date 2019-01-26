ff187fb3bef093968da04b416cea076c6c52bc49
Tom <tomn98@live.unc.edu>
2019-01-26 14:14:13 -0500

    Basic styling changes

--- scripts/messageContainer.js
+++ scripts/messageContainer.js
@@ -3,9 +3,9 @@
 var createMessage = function(isComputer, message) {
     var messageLabel;
     if (isComputer === true){
-        messageLabel =  $('<div class="messageCPU"></div>');
+        messageLabel =  $('<div class="message CPUmsg"></div>');
     } else {
-        messageLabel = $('<div class="messageUSER"></div>');
+        messageLabel = $('<div class="message USERmsg"></div>');
     }
 
     messageLabel.append(message);
