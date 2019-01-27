458602c0c277af1686627aa64ccdb9d0990cbaae
Sophia Shaikh <sophias930@gmail.com>
2019-01-27 15:02:36 -0500

    embedded audio

--- scripts/soundEngine.js
+++ scripts/soundEngine.js
@@ -1,7 +1,21 @@
 var audio;
 var playMessageSound = function (message){
-    if (message === "*phone ringing*"){
+    if (message === "*phone ringing* "){
+        /*
         audio = new Audio("music/phone_ringing.mp3");
         audio.play();   
+        */
+       console.log("audio about to play");
+       var x = document.getElementById("phone_ringing");
+       x.play();
     };
 }
+
+// *phone ringing* --> phone_ringing.mp3
+// *click* --> phone_pickedup.mp3
+// *whizzing noise* --> power_whizzing.mp3
+// *buzzing noise and door opening* --> door_buzz.mp3
+// *loud whirring followed by a loud thud* --> barricade_thudding.mp3
+// *phone line dead noise* --> dead_phone.mp3
+// *wiping noise* --> wiping_mirror.mp3
+// *door opening* --> door_opening.mp3
