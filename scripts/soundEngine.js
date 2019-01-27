a3412ec149f4fa5c4de72a681b080436657bfeef
Sophia Shaikh <sophias930@gmail.com>
2019-01-27 15:34:58 -0500

    UGH

--- scripts/soundEngine.js
+++ scripts/soundEngine.js
@@ -1,15 +1,39 @@
-var playMessageSound = function (message){
-<<<<<<< HEAD
-    if (message === "*phone ringing* "){
+var playMessageSound = function (message) {
+    console.log(message);
+    if (message === "C: *phone ringing* ") {
         /*
         audio = new Audio("music/phone_ringing.mp3");
         audio.play();   
         */
-       console.log("audio about to play");
-       var x = document.getElementById("phone_ringing");
-       x.play();
+        console.log("audio about to play");
+        var music = document.getElementById("phone_ringing");
+
+        
+        var promise = music.play();
+        if (promise !== undefined) {
+            promise.then(_ => {
+                console.log("no error");
+                music.play();
+            }).catch(error => {
+                console.log(error);
+            });
+        }
+        
+        //playMusic(music);
+
     };
 }
+
+async function playMusic(music) {
+    try {
+        console.log("about to play!");
+        await music.play();
+        console.log("playing...");
+      } catch(err) {
+        console.log(err);
+      }
+}
+
 
 // *phone ringing* --> phone_ringing.mp3
 // *click* --> phone_pickedup.mp3
@@ -19,10 +43,10 @@ var playMessageSound = function (message){
 // *phone line dead noise* --> dead_phone.mp3
 // *wiping noise* --> wiping_mirror.mp3
 // *door opening* --> door_opening.mp3
-=======
+/*
     console.log(message);
     if (message === "C: *phone ringing* "){
-        
+
     }
-}
->>>>>>> 6c3f5bb728ef8df4aa9e60b73e1aeb418e1c4bd1
+    */
+
